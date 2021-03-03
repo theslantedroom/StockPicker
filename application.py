238ebf7f7508fdfb241ea959703e4c2cc770bfac
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-02 16:59:05 -0800

    push update

---
+++ application.py
@@ -0,0 +1,201 @@
+import os
+
+# for my local env variable and api key
+from dotenv import load_dotenv
+project_folder = os.path.expanduser('./')  # adjust as appropriate
+load_dotenv(os.path.join(project_folder, '.envVar'))
+
+from cs50 import SQL
+from flask import Flask, flash, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+from helpers import apology, login_required, lookup, usd
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+# Custom filter
+app.jinja_env.filters["usd"] = usd
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///finance.db")
+
+# Make sure API key is set
+if not os.environ.get("API_KEY"):
+    raise RuntimeError("API_KEY not set")
+# print(os.environ['API_KEY'])
+os.environ["DEBUSSY"] = "1"
+
+
+@app.route("/")
+@login_required
+def index():
+    """Show portfolio of stocks"""
+    return apology("TODO")
+
+
+@app.route("/buy", methods=["GET", "POST"])
+@login_required
+def buy():
+    """Buy shares of stock"""
+    if request.method == "GET":
+        return render_template("buy.html")
+    if request.method == "POST":
+        return apology("buy")
+    # return apology("TODO")
+
+@app.route("/history")
+@login_required
+def history():
+    """Show history of transactions"""
+    return apology("TODO")
+
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+
+    # Forget any user_id
+    session.clear()
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+
+        # Ensure username was submitted
+        if not request.form.get("username"):
+            return apology("must provide username", 403)
+
+        # Ensure password was submitted
+        elif not request.form.get("password"):
+            return apology("must provide password", 403)
+
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
+            return apology("invalid username and/or password", 403)
+
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
+
+        # Redirect user to home page
+        return redirect("/")
+
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
+
+
+@app.route("/logout")
+def logout():
+    """Log user out"""
+
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
+
+@app.route("/quote", methods=["GET", "POST"])
+@login_required
+def quote():
+    """Get stock quote."""
+    if request.method == "GET":
+        return render_template("quote.html", stock="")
+
+    if request.method == "POST":
+        stock = lookup(request.form.get("stock"))
+        if not stock:
+            return apology("That stock does not exist")
+
+        return render_template("quote.html", stock=stock)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "GET":
+        return render_template("register.html")
+
+    if request.method == "POST":
+        username = request.form.get('username')
+        registerpassword1 = request.form.get('registerpassword1')
+        registerpassword2 = request.form.get('registerpassword2')
+        PWhash = generate_password_hash(registerpassword1)
+        registeredUsers = db.execute("SELECT username FROM users")
+    
+        print(registeredUsers)
+
+        if not username:
+            return apology("no username")
+        if not registerpassword1:
+            return apology("did not enter password")
+        if not registerpassword2:
+            return apology("did not enter retyped password")
+        if registerpassword1 != registerpassword2 :
+            return apology("passwords did not match")
+
+        if db.execute("SELECT * FROM users WHERE username = :username",
+            username=request.form.get("username")):
+            return apology("Username taken")
+
+        db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", username, PWhash)
+
+        return redirect("/")
+
+
+    
+
+
+@app.route("/sell", methods=["GET", "POST"])
+@login_required
+def sell():
+    """Sell shares of stock"""
+    if request.method == "GET":
+        stocks = {}
+        return render_template("sell.html", stocks=stocks)
+    if request.method == "POST":
+        return apology("sell")
+    # return apology("TODO")
+
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+# Listen for errors
+for code in default_exceptions:
+    app.errorhandler(code)(errorhandler)
+
+# must run in cmd venv before start
+# $Env:FLASK_APP="application.py"
+# $Env:FLASK_DEBUG=1
+
+# C:\sqlite\sqlite-tools-win32-x86-3340100\sqlite3.exe finance.db
