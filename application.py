dbdbe2a874dc51cc277e48a7fffd1eef070a223c
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-03 10:57:00 -0800

    push update

--- application.py
+++ application.py
@@ -386,12 +386,12 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 # for development
-if __name__ == '__main__':
-    app.run(debug=True)
+# if __name__ == '__main__':
+#     app.run(debug=True)
 
 
 # for production
-# if __name__ == '__main__':
-#  app.debug = True
-#  port = int(os.environ.get("PORT", 8080))
-#  app.run(host="0.0.0.0", port=port)
+if __name__ == '__main__':
+ app.debug = True
+ port = int(os.environ.get("PORT", 8080))
+ app.run(host="0.0.0.0", port=port)
