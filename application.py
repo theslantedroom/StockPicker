55287f8066fc107fbb8954f8092e131dc9fa0c74
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-02 18:17:05 -0800

    push update

--- application.py
+++ application.py
@@ -207,10 +207,10 @@ def errorhandler(e):
 for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
 # if __name__ == '__main__':
-#  app.debug = True
-#  port = int(os.environ.get("PORT", 8080))
-#  app.run(host="0.0.0.0", port=port)
+#     app.run(debug=True)
+
+if __name__ == '__main__':
+ app.debug = True
+ port = int(os.environ.get("PORT", 8080))
+ app.run(host="0.0.0.0", port=port)
