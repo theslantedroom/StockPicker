3f41016c7dd03b52302a5443b86acaa9312d12a7
theslantedroom <73557008+theslantedroom@users.noreply.github.com>
2021-03-03 11:02:13 -0800

    push update

--- application.py
+++ application.py
@@ -390,6 +390,6 @@ for code in default_exceptions:
 
 # for production
 if __name__ == '__main__':
- app.debug = True
+ app.debug = False
  port = int(os.environ.get("PORT", 8080))
  app.run(host="0.0.0.0", port=port)
